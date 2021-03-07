99fb95c425af33df71580b1442d58352f7a0407e
sinbox0701 <psh090953@gmail.com>
2021-03-07 14:51:33 +0900

    Login Page Center 안맞음

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,20 +1,92 @@
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import { darkModeVar, isLoggedInVar } from "../apollo";
 
-const Title = styled.h1`
-    color: ${(props)=>props.theme.fontColor};
+const Container = styled.div`
+    display:flex;
+    height: 100vh;
+    jusify-content:center;
+    align-items:center;
+    flex-direction:column;
 `;
 
-const Container = styled.div`
-    background-color: ${(props)=>props.theme.bgColor};
+const whiteBox = styled.div`
+    background-color: white;
+    border: 1px solid rgb(219,219,219);
+    width:100%;
+`;
+
+const TopBox = styled(whiteBox)`
+    display:flex;
+    jusify-content:center;
+    align-items:center;
+    flex-direction: column;
+    padding: 35px 40px 25px 40px;
+    form{
+        margin-top:35px;
+        width: 100%;
+        display:flex;
+        jusify-items:center;
+        flex-direction: column;
+        align-items:center;
+        input{
+            width:100%;
+            border-radius: 3px;
+            padding: 7px;
+            background-color:#fafafa;
+            border: 0.5px solid rgb(219,219,219);
+            margin-top:5px;
+            box-sizing:border-box;
+            &::placeholder{
+                font-size:12px;
+            }
+            &:last-child{
+                border:none;
+                margin-top:12px;
+                background-color:#0095F6;
+                color: white;
+                text-align:center;
+                padding: 8px 0px;
+                font-weight: 600;
+            }
+        }
+    }
+`;
+
+const BottomBox = styled(whiteBox)`
+    padding: 20px; 0px;
+    text-align: center;
+    a{
+        font-weight:600;
+        color:#0095F6;
+        font-family:'Open Sans', sans-serif;
+    }
+`;
+
+const Wrapper = styled.div`
+    max-width:350px;
+    width: 100%;
 `;
 
 const Login = () => {
     return (
         <Container>
-            <Title>Login</Title>
-            <button onClick={() => darkModeVar(true)}>Todark</button>
-            <button onClick={() => darkModeVar(false)}>Tolight</button>
+            <Wrapper>
+                <TopBox>
+                    <div>
+                        <FontAwesomeIcon icon={faInstagram} size="3x"/>
+                    </div>
+                    <form>
+                        <input type="text" placeholder="학번"/>
+                        <input type="password" placeholder="비밀번호"/>
+                        <input type="submit" value="Log In"/>
+                    </form>
+                </TopBox>
+                <BottomBox>
+                    <span>Don't have an account?</span><a href="#">Sign Up</a>
+                </BottomBox>
+            </Wrapper>
         </Container>
     )
 }
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -4,22 +4,17 @@ import reset from "styled-reset";
 //css 초기화
 export default createGlobalStyle`
     ${reset};
-    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
+    input{
+        all:unset;
+    }
     *{
         box-sizing:border-box;
     }
     body {
-        background-color:${props => props.theme.bgColor};
-        color:${props => props.theme.blackColor};
+        background-color: #FAFAFA;
         font-size:14px;
-        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        padding-top: 140px;
     }
     a{
-        color:${props => props.theme.blueColor};
         text-decoration:none;
-    }
-    input:focus{
-        outline:none;
     }
 `;
